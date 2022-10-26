2a7a8b4e308e8142e46c6882840f8191ff3fd9c5
TretyakovKonstantin <spb_unex@mail.ru>
2022-10-26 15:58:53 +0300

    Выполнено задание №2

--- src/Human.java
+++ src/Human.java
@@ -3,16 +3,24 @@ public class Human {
     long yearOfBirth;
     private String name;
     String town;
+    String jobTitle;
 
     Human(String name) {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
+    public String toStringTask1() {
         return "Привет! Меня зовут " + name + ". " +
                 "Я из города " + town + ". " +
                 "Я родился в " + yearOfBirth + " году. " +
+                "Будем знакомы!";
+    }
+
+    public String toStringTask2() {
+        return "Привет! Меня зовут " + name + ". " +
+                "Я из города " + town + ". " +
+                "Я родился в " + yearOfBirth + " году. " +
+                "Я работаю на должности " + jobTitle + ". " +
                 "Будем знакомы!";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        task1();
-    }
 
-    public static void task1() {
-        System.out.println();
-        System.out.println("Задание №1");
+    static Human[] arrHumans;
+
+    public static void main(String[] args) {
+
+        arrHumans = new Human[4];
 
         Human maksim = new Human("Максим");
         maksim.yearOfBirth = 1988;
@@ -23,12 +22,43 @@ public class Main {
         artem.yearOfBirth = 1995;
         artem.town = "Москва";
 
-        System.out.println(maksim.toString());
-        System.out.println(any.toString());
-        System.out.println(katy.toString());
-        System.out.println(artem.toString());
+        arrHumans[0] = maksim;
+        arrHumans[1] = any;
+        arrHumans[2] = katy;
+        arrHumans[3] = artem;
 
+        task1();
+        task2();
+    }
 
+    public static void task1() {
+        System.out.println();
+        System.out.println("Задание №1");
+
+        for (int i = 0; i < arrHumans.length; i++) {
+            System.out.println(arrHumans[i].toStringTask1());
+        }
+
+    }
+
+    public static void task2() {
+        System.out.println();
+        System.out.println("Задание №2");
+
+        Human maksim = arrHumans[0];
+        Human any = arrHumans[1];
+        Human katy = arrHumans[2];
+        Human artem = arrHumans[3];
+
+        maksim.jobTitle = "бренд-менеджер";
+        any.jobTitle = "методист образовательных программ";
+        katy.jobTitle = "продакт-менеджером";
+        artem.jobTitle = "директор по развитию бизнеса";
+
+        // так мы имееем только ссылку на объект класс, то изменив объект мы изменили ег ои в массиве
+        for (int i = 0; i < arrHumans.length; i++) {
+            System.out.println(arrHumans[i].toStringTask2());
+        }
 
     }
 
